52dddd725e0ac42e01349a2c44b761a03b753963
Wizaniac <dugartushar1@gmail.com>
2021-11-30 15:21:08 +0530

    Added user credentials(employeeid as number) in AddUpdate function and storing data with credentials in api

--- FlaskTest.py
+++ FlaskTest.py
@@ -3,46 +3,7 @@ app = Flask(__name__)
 
 # Characters
 # Wheel of Time
-db = [{'id': "0", 'name': "Moiraine Sedai", 'gender': "Female",
-       'description': "Moiraine says she has come to the Two Rivers in search of the one who will save or destroy the "
-                      "world — but ask of her intentions and you’ll be met with cold stares and heavy silence. Aes "
-                      "Sedai may never speak that which is not true, making her silence all the more concerning. Will "
-                      "she protect the Dragon Reborn, whomever they may be, or are her intentions more nefarious?"},
-      {'id': "1", 'name': "Egwene", 'gender': "Female",
-       'description': "Innkeeper’s daughter, promised to Rand al’Thor, apprentice to Nynaeve... It all seems "
-                      "inconsequential in light of the possibilities that lay before Egwene. Could she truly be a "
-                      "powerful channeler, and perhaps even the Dragon Reborn? Whether or not what Moiraine suggests "
-                      "is true, it’s beginning to seem that Egwene is far more powerful — and more important — than "
-                      "she could have ever imagined."},
-      {'id': "2", 'name': "Rand", 'gender': "Male",
-       'description': "Rand doesn’t trust Moiraine. Her veiled intentions, and the forces of evil that seem to follow "
-                      "her, make her a threat in his eyes. He is brash, impulsive, and impossibly stubborn, "
-                      "but he’s driven to protect those he loves. If he’s not the one Moiraine seeks, he will still "
-                      "do everything in his power to save them from whatever she has planned. But if he is, "
-                      "perhaps he has the power to protect them all."},
-      {'id': "3", 'name': "Mat", 'gender': "Male",
-       'description': "Most see Mat as a witty, mischievous rogue — which he certainly is — but deep down, Mat fears "
-                      "he’s a bastard like his father; a drunk like his mother. If this Dragon Reborn business comes "
-                      "with responsibility, he’s not interested. But perhaps, whether or not he’s who Moiraine is "
-                      "looking for, it can be his ticket to a better life."},
-      {'id': "4", 'name': "Perrin", 'gender': "Male",
-       'description': "Perrin is a kind, gentle soul woven into a powerful and physically imposing form. He takes as "
-                      "much care in forming his words and actions as he would a fine blade on his forge. He strives "
-                      "to avoid inadvertently harming others — physically or emotionally — yet his anger, "
-                      "when provoked, is truly terrifying. Perrin has yet to unleash the full ferocity of his power, "
-                      "and he hopes to keep it that way."},
-      {'id': "5", 'name': "Nynaeve", 'gender': "Female",
-       'description': "Nynaeve’s stubborn nature and brash self-righteousness were necessary tools of the trade when "
-                      "she became the youngest Wisdom the Two Rivers had ever seen. And now it seems her inherent "
-                      "ability to Listen to the Wind and profound aptitude for healing may be mere glimpses of her "
-                      "true potential. She has no trust for Aes Sedai, but she will do what she must to find the "
-                      "others from the Two Rivers and keep them safe."},
-      {'id': "6", 'name': "Lan Mandragoran", 'gender': "Male",
-       'description': "Lan is a man of honor, driven by duty. As Moiraine’s Warder, his commitment to her quest is "
-                      "unwavering. Were you somehow able to gain his trust, you would find his stoic face and "
-                      "propensity for silence mask an endearing depth and complexity of character. Still, "
-                      "it’s best not to let your guard down around him. After all, there are few things more "
-                      "formidable than an Aes Sedai and her Warder."}]
+db = [{}]
 
 
 @app.route('/')
--- datasender.py
+++ datasender.py
@@ -1,16 +1,17 @@
 import requests
 import json
 
-def AddUpdate(dataa):
+def AddUpdate(id,dataa):
     data_endpoint = "http://127.0.0.1:5000/database"
     dataa = json.dumps(dataa)
+    print(dataa)
     resp = requests.post(url=data_endpoint, data=dataa)
     if resp.status_code == 200:
         return eval(resp.text)
     else:
         return "Your api has some problem. Status" + str(resp.status_code)
 
-def DispData():
+def DispData(id):
     data_endpoint = "http://127.0.0.1:5000/database"
     resp = requests.get(url=data_endpoint)
     return eval(resp.text)
