249edc0fc9591f2632a961530c30c89060568028
Wizaniac <dugartushar1@gmail.com>
2021-12-02 22:04:05 +0530

    Prettified the output in Json format

--- Test.py
+++ Test.py
@@ -2,12 +2,12 @@ import requests
 import json
 from datasender import *
 
-with open('Students.json') as json_file:
+with open('data.json') as json_file:
     data_toSend = json.load(json_file)
 
 #Enter a user id in numerals, data as second argument of the AddUpdate function like AddUpdate(123,data_toSend)
-print(AddUpdate(123,data_toSend))
+#print(AddUpdate(456,data_toSend))
 
 # For display of dataset and logs, enter in the following format - DispData(userid,"dd/mm/yyyy")
-print(DispData(123,"02/11/2021"))
+print(DispData(456,"02/11/2021"))
 
