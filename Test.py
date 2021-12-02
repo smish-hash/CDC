30df6137763a9f4063f7c042120036e5ed590e05
Satyajit Mishra <55646021+smish-hash@users.noreply.github.com>
2021-12-02 21:30:43 +0530

    Working log

--- Test.py
+++ Test.py
@@ -3,5 +3,5 @@ from datasender import *
 with open('Students.json') as json_file:
     data_toSend = json.load(json_file)
 
-print(AddUpdate(456, data_toSend))
+print(AddUpdate(567, data_toSend))
 print(DispData())
